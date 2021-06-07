2de0201d9d492ad7ea29d8d38ced62cda3274e2f
Bruno Fernandes <bfgarcia94@gmail.com>
2021-06-07 00:24:51 -0300

    contexto de filmes

---
+++ src/contexts/MoviesContext.js
@@ -0,0 +1,61 @@
+import React, { useCallback, useMemo, useReducer } from 'react'
+
+import fetchMoviesByQuery from '../services/fetchMoviesByQuery'
+
+const MoviesContext = React.createContext()
+
+const MoviesConsumer = MoviesContext.Consumer
+
+const initialState = {
+    moviesList: [],
+    moviesListStatus: '',
+}
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'setMoviesList':
+			return { ...state, moviesList: action.payload, moviesListStatus: 'loaded' }
+		case 'setMoviesListStatus':
+			return { ...state, moviesListStatus: action.payload }
+		default:
+			throw new Error()
+	}
+}
+
+const MoviesProvider = ({ children }) => {
+	const [state, dispatchMovies] = useReducer(reducer, initialState)
+
+	const loadMoviesList = useCallback((name) => {
+		dispatchMovies({ type: 'setMoviesListStatus', payload: 'loading' })
+		fetchMoviesByQuery(name)
+			.then((response) => {
+				dispatchMovies({ type: 'setMoviesList', payload: response })
+			})
+			.catch((error) => {
+				MoviesContext({ type: 'setMoviesListStatus', payload: 'error' })
+                console.log(error)
+			})
+	}, [])
+
+	console.log(state)
+
+	const providerValue = useMemo(() => ({
+			...state,
+			loadMoviesList,
+		}), 
+		[
+			loadMoviesList, 
+			state,
+		])
+	return (
+		<MoviesContext.Provider value={providerValue}>
+			{children}
+		</MoviesContext.Provider>
+	)
+}
+
+export {
+  MoviesConsumer,
+  MoviesContext as default,
+  MoviesProvider,
+}
