c5236b9fb8f3d5af5b6e89833d61e61c7ff2b8b1
Bruno Fernandes <bfgarcia94@gmail.com>
2021-06-07 00:24:28 -0300

    arquivo de rotas

---
+++ src/Routes.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './views/Home'
+import MovieInfo from './views/MovieInfo'
+import PageNotFound from './views/PageNotFound'
+
+const Routes = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/movie">Movie Info</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/movie">
+            <MovieInfo />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default Routes
