b9a0b649b9799fc285345faa15f950c7a76fbc11
Bruno Fernandes <bfgarcia94@gmail.com>
2021-06-10 23:57:50 -0300

    componente de loading

---
+++ src/components/LoadingHandler/LoadingHandler.js
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import StyledLoadingHandler from './StyledLoadingHandler'
+
+const LoadingHandler = ({
+  children,
+  dataStatus,
+  errorMessage,
+  loadingMessage,
+}) => (
+  <StyledLoadingHandler>
+    {dataStatus === 'loaded' && (
+      <>{children}</>
+    )}
+    {dataStatus === 'loading' && (
+      <div className="message">{loadingMessage}</div>
+    )}
+    {dataStatus === 'error' && (
+      <div className="message">{errorMessage}</div>
+    )}
+  </StyledLoadingHandler>
+)
+
+LoadingHandler.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  dataStatus: PropTypes.string,
+  errorMessage: PropTypes.string,
+  hasData: PropTypes.bool,
+  loadingMessage: PropTypes.string,
+  noDataMessage: PropTypes.string,
+}
+
+LoadingHandler.defaultProps = {
+  dataStatus: '',
+  errorMessage: 'Erro ao carregar dados',
+  loadingMessage: 'Carregando...',
+}
+
+export default LoadingHandler
---
+++ src/components/LoadingHandler/StyledLoadingHandler.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const StyledLoadingHandler = styled.div`
+  .message {
+    padding: 10px;
+    text-align: center;
+  }
+`
+
+export default StyledLoadingHandler
