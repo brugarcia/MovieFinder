1b83082f226287af0ff752c180d26b587a9f5200
Bruno Fernandes <bfgarcia94@gmail.com>
2021-06-11 12:29:34 -0300

    rotas

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom"
 
 import Home from './views/Home'
@@ -14,17 +13,6 @@ const Routes = () => {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/movie">Movie Info</Link>
-            </li>
-          </ul>
-        </nav>
-
         <Switch>
           <Route path="/movie">
             <MovieInfo />
