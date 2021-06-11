cf8b9372a552f98a6249025712858c3d396b5553
Bruno Fernandes <bfgarcia94@gmail.com>
2021-06-11 12:29:12 -0300

    correcao de chave unica para lista de filmes

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,22 +1,22 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext} from 'react'
 
 import MoviesContext from '../../contexts/MoviesContext'
 
-const initState = {
-	moviesList: []
-}
+// const initState = {
+// 	moviesList: []
+// }
 
 
 const MovieList = () => {
-	const [state, setState] = useState(initState)
+	//const [state, setState] = useState(initState)
     const {
 		moviesList,
 	 } = useContext(MoviesContext)
 
 	return (
         <>
-			{moviesList.results?.map(({title}) => (
-				<div key={title}>
+			{moviesList.results?.map(({id, title}) => (
+				<div key={id}>
 					{title}
 				</div>
 			))}
