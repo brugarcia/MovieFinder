481881d6b8bb6f500503a4a65387f11e5ebd12db
Bruno Fernandes <bfgarcia94@gmail.com>
2021-06-11 12:29:57 -0300

    ajuste estilo

--- src/components/SearchBar/StyledSearchBar.js
+++ src/components/SearchBar/StyledSearchBar.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 
 const StyledSearchBar = styled.div`
-  & input {
-    color: red;
-    width: 100%;
-  }
+  margin: 20px 20px;
+
+    & input {
+      color: red;
+      width: 100%;
+    }
 `
 
 export default StyledSearchBar
