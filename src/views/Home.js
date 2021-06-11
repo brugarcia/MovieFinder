37748a52e51bc8f80b6217d2baf7641a6dd6ef55
Bruno Fernandes <bfgarcia94@gmail.com>
2021-06-10 23:58:25 -0300

    pesquisa, loading e lista em home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import SearchBar from '../components/SearchBar'
 import MovieList from '../components/MovieList'
+import LoadingHandler from '../components/LoadingHandler'
+
+import MoviesContext from '../contexts/MoviesContext'
 
 
 
 const Home = () => {
 
+  const {
+    moviesListStatus,
+	 } = useContext(MoviesContext)
+
 	return	(
 		<>
 			<SearchBar/>
-			<MovieList/>
+      <LoadingHandler dataStatus={moviesListStatus}>
+        <MovieList/>
+      </LoadingHandler>
+
 		</>
 	)
 }
-	
 
-	
+
+
 
 
 export default Home
