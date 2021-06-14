415a06da46bc2abc44525671ef8c03e7f51711a1
Bruno Fernandes <bfgarcia94@gmail.com>
2021-06-14 12:29:31 -0300

    detalhes de filmes

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -12,12 +12,41 @@ const MovieDetails = () => {
   const {
     original_title: originalTitle,
     poster_path: imagePath,
+    title,
+    overview,
+    release_date: date,
+    runtime,
+    vote_average: rating,
   } = movie;
 
   return (
     <StyledMovieDetails>
-      {originalTitle}
-      <ImageWrapper name={originalTitle} path={imagePath} size={500} />
+      <span className="infos">
+        <h1 className="title">
+          {title}
+        </h1>
+
+        <span className="mini-infos">
+          <div className="date">
+            {date.split('-', 1)}
+          </div>
+          <div className="runtime">
+            {`${runtime} min`}
+          </div>
+        </span>
+
+        <p className="overview">
+          {overview}
+        </p>
+
+        <div className="rating">
+          {`Nota: ${rating}`}
+        </div>
+      </span>
+
+      <span className="movie-image">
+        <ImageWrapper name={originalTitle} path={imagePath} size={400} />
+      </span>
     </StyledMovieDetails>
   );
 };
--- src/components/MovieDetails/StyledMovieDetails.js
+++ src/components/MovieDetails/StyledMovieDetails.js
@@ -1,7 +1,35 @@
 import styled from 'styled-components';
 
 const StyledMovieDetails = styled.div`
+  display: flex;
+  flex-direction: row;
   color: red;
+  justify-content: space-between;
+
+  .infos {
+    margin: 30px 50px;
+  }
+
+  .mini-infos {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .date {
+    margin-right: 10px
+  }
+
+  .title {
+    font-size: 40px;
+  }
+
+  .overview {
+    color: #fff
+  }
+
+  .movie-image {
+    margin: 30px 50px;
+  }
 `;
 
 export default StyledMovieDetails;
